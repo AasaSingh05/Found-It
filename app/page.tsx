e067860b7a11544ec04d33840b8ecf83993877c8
AasaSingh05 <aasasingh2005@gmail.com>
2024-11-06 20:02:31 +0530

    fix: we added the proper configuration, and the fixed the tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -2,4 +2,7 @@ import { redirect } from 'next/navigation'
  
 export default function Page() {
   redirect('/landing')
+  return (
+    <p>yay this will never be seen :)</p>
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,14 +2,13 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
-        cardcol: '#28AFB0',
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
@@ -18,5 +17,3 @@ const config: Config = {
   plugins: [],
 };
 export default config;
-
-
